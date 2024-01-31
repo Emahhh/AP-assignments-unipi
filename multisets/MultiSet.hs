c173b7fbdbaad6a9196136871b7bd3f417c10432
Emanuele B. <buonaccorsiemanuele@gmail.com>
2024-01-31 19:19:51 +0100

    finished implementing

--- multisets/MultiSet.hs
+++ multisets/MultiSet.hs
@@ -38,34 +38,57 @@ occs (MSet mset) v =
 
 -- returns a list containing all the elements in `mset`
 elems :: Mset a -> [a] 
-elems (MSet mset)
+elems (MSet mset) =
+    foldl util [] mset
+    where util acc (currKey, _) = currKey:acc -- I add the current element to the list
 
 -- returns `True` iff each element of `mset1` is also an element of `mset2`, with at least the same multiplicity
 subeq :: Eq a => (Mset a) -> (Mset a) -> Bool
-subeq mset1 mset2
+subeq mset1 mset2 =
+    case mset1 of
+        [] -> True
+        (currKey, currMult):rest -> if currMult > occs mset2 currKey then False else subeq rest mset2
+
+
+-- additional function (not required by the exercise) added to implement the next one
+-- runs the `add` function `n` times on value `v`
+addNTimes v n (MS mset) =
+    if n <= 0 then mset
+    else addNTimes v (n-1) (add mset v)
+
 
 -- returns a MSet having all elements of `mset1` and of `mset2`, each with the sum of the corresponding multiplicities
+-- to implement this, I add to `mset1` each element of `mset2`, as many times as it occurs in `mset2`
 union :: Eq a => (Mset a) -> (Mset a) -> (Mset a)
-union mset1 mset2
+union mset1 mset2 =
+    map util mset1
+    where util (currKey, currMult) = addNTimes mset2 currKey currMult
 
 
 
 
 -- CLASS CONSTRUCTOR INSTANCES -----------
--- TODO: controllare documentazione per instance
 
 
 -- Defining MSet to be an instance of Eq. This means implementing the equality operation on `MSet`.
--- I can only do this if the type a itself has an instance of Eq.
+-- I can only do this if the type a itself has an instance of Eq (`\=` is derived from `==`).
 -- equality is defined as: two multisets are equal iff they contain the same elements with the same multiplicities
 instance Eq a => Eq (MSet a) where
     (==) :: Eq a => MSet a -> MSet a -> Bool
     (==) mset1 mset2 = subeq mset1 mset2 && subeq mset2 mset1
 
 
+-- Defining MSet to be an instance of Foldable.
+-- implementing `foldr` is enough (minimal set of fucntion to be implemented according to the documentation of `Foldable`)
+-- TODO: comment
 instance Foldable (MSet a)
+    foldr fun acc (MS []) = acc
+    foldr fun acc (MS (key,_):rest) = fun key (foldr fun acc (MS rest))
 
 
--- Explain (in a comment in the same file) why it is not possible to define an instance of Functor for MSet by providing mapMSet as the implementation of fmap.
+-- TODO: Explain (in a comment in the same file) why it is not possible to define an instance of Functor for MSet by providing mapMSet as the implementation of fmap.
 mapMSet :: (a -> b) -> (MSet a) -> (MSet b)
-mapMSet fun mset 
+mapMSet fun mset =
+    case mset of
+        MS [] -> MS []
+        (currKey, currMult):rest -> MS [(fun currKey, currMult) ++ (mapMSet fun rest)]
