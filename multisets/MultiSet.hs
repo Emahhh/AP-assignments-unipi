6080c7de6f78f3427f22c2ff587f4119885e0973
Emanuele B. <buonaccorsiemanuele@gmail.com>
2024-01-31 15:34:54 +0100

    implemented first 2

--- multisets/MultiSet.hs
+++ multisets/MultiSet.hs
@@ -15,13 +15,26 @@ empty = MS []
 -- OPERATIONS --------------------------------
 
 -- returns a multiset obtained by adding the element `v` to `mset`
--- added the costraint `Eq a` to the type signature so that we can compare elements (to check if already present)
+-- added the costraint `Eq a` to the type signature so that we are sure we can compare elements (to check if already present)
 add :: Eq a => (MSet a) -> a -> (MSet a) 
-add (MSet mset) v = MS [] -- TODO: complete
+add (MSet mset) v =
+    -- check if element already present
+    if elem v (map fst mset) then
+        MSet (map incrementEntry v mset) -- if element already present, increment its multiplicity
+    else
+        MSet (mset ++ [(v, 1)]) -- if element not present, I concatenate a new entry
+
+    where incrementEntry v (currKey, currVal) =
+        if currKey == v then (currKey, currVal + 1) else (currKey, currVal)
+
+
 
 -- returns the number of occurrences of `v` in `mset`
 occs :: Eq a => (MSet a) -> a -> Int
-occs (MSet mset) v
+occs (MSet mset) v = 
+    case mset of
+        [] -> 0 -- if mset is empty, I found 0 occurrences of v
+        (currVal, currMult):rest -> if currVal == v then currMult else occs (MSet rest) v -- I check if the current element is the one I'm looking for
 
 -- returns a list containing all the elements in `mset`
 elems :: Mset a -> [a] 
@@ -48,3 +61,11 @@ union mset1 mset2
 instance Eq a => Eq (MSet a) where
     (==) :: Eq a => MSet a -> MSet a -> Bool
     (==) mset1 mset2 = subeq mset1 mset2 && subeq mset2 mset1
+
+
+instance Foldable (MSet a)
+
+
+-- Explain (in a comment in the same file) why it is not possible to define an instance of Functor for MSet by providing mapMSet as the implementation of fmap.
+mapMSet :: (a -> b) -> (MSet a) -> (MSet b)
+mapMSet fun mset 
