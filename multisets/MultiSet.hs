2def07b8f8debef40fc8a141ca88ab1b16000670
Emanuele B. <buonaccorsiemanuele@gmail.com>
2024-01-31 13:04:18 +0100

    wrote 2 signatures

--- multisets/MultiSet.hs
+++ multisets/MultiSet.hs
@@ -1,6 +1,21 @@
 module MultiSet where
+-- exercise: implement a type constructor providing the functionalities of multisets (also known as bags)
+-- that is, collections of elements where the order does not count, but each element can occur several times.
 
+-- CONSTRUCTOR provided by the exercise
+-- `MS` is the constructor for the type `MSet`
 data MSet a = MS [(a, Int)] deriving (Show)
 
-main = do
-    putStrLn "Hello, World!"
+-- empty constructor
+-- returns an empty multiset
+empty :: MSet a
+empty = MS []
+
+-- returns a multiset obtained by adding the element `v` to `mset`
+-- added the costraint `Eq a` to the type signature so that we can compare elements (to check if already present)
+add :: Eq a => (MSet a) -> a -> (MSet a) 
+add (MSet mset) v = MS [] -- TODO: complete
+
+-- returns the number of occurrences of `v` in `mset`
+occs :: Eq a => (MSet a) -> a -> Int
+occs (MSet mset) v
--- multisets/TestMSet.hs
+++ multisets/TestMSet.hs
@@ -1,5 +1,10 @@
 module TestMSet where
+import MultiSet
 
+esempioSet :: MSet Int
+esempioSet = MS [(1, 3), (2, 2), (3, 1)]
 
+main :: IO ()
 main = do
-    putStrLn "Hello, World!"
+    putStrLn "Hello, TestMSet!"
+    print esempioSet
